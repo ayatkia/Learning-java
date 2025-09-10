1892f629e202304f767f5ac6b2001766b5b2b583
Ayat <a.kyanejad@yahoo.com>
2025-09-11 00:11:42 +0330

    first commit

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ import staff.Employer;
 import staff.Waiter;
 
 import java.sql.Array;
+import java.util.Arrays;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -12,10 +13,24 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int[] arry={4,2,55,6,7,9,25,1,3};
+        /// //////////////Sort///////////////////////
 //        Sort1 sort1 = new Sort1(arry);
 //        sort1.sorting();
-        Selectionsort selectionsort=new Selectionsort(arry);
-        selectionsort.sorting();
+//        Selectionsort selectionsort=new Selectionsort(arry);
+//        selectionsort.sorting();
+        /// /////////////ArraySort/////////////
+//        LinearSearch linearSearch = new LinearSearch();
+//       int x= linearSearch.search(arry,9);
+//       System.out.println(x);
+        ///  ///////InsertArray///////////////
+        InsertOperation insertOperation = new InsertOperation();
+         arry= insertOperation.InsertionSort(arry,4,100);
+
+            System.out.print(Arrays.toString(arry));
+
+
+
+
 
 
 
