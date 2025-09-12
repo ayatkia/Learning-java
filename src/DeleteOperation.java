43119706da2447f1e9d56c5837c56d38588779ac
Ayat <a.kyanejad@yahoo.com>
2025-09-12 22:03:37 +0330

    first commit

--- src/DeleteOperation.java
+++ src/DeleteOperation.java
@@ -1,2 +1,16 @@
 public class DeleteOperation {
+    public int[] DeleteionSort(int[] arry,int pos) {
+        int[] newArray= new int[arry.length-1];
+        for(int i=0;i<pos;i++){
+            newArray[i]=arry[i];
+        }
+
+        for(int i=pos;i<newArray.length;i++){
+            newArray[i]=arry[i+1];
+        }
+
+        return newArray;
+    }
 }
+
+
--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int[] arry={4,2,55,6,7,9,25,1,3};
+        int[] arry = {4, 2, 55, 6, 7, 9, 25, 1, 3};
         /// //////////////Sort///////////////////////
 //        Sort1 sort1 = new Sort1(arry);
 //        sort1.sorting();
@@ -23,20 +23,35 @@ public class Main {
 //       int x= linearSearch.search(arry,9);
 //       System.out.println(x);
         ///  ///////InsertArray///////////////
-        InsertOperation insertOperation = new InsertOperation();
-         arry= insertOperation.InsertionSort(arry,4,100);
+//        InsertOperation insertOperation = new InsertOperation();
+//         arry= insertOperation.InsertionSort(arry,4,100);
+//
+//            System.out.print(Arrays.toString(arry));
+        ///  ///////DeleteArray///////////////
+//        DeleteOperation delete = new DeleteOperation();
+//        arry=delete.DeleteionSort(arry,4);
+//        System.out.print(Arrays.toString(arry));
+        ///  ///////PalindromeCheck ///////////////
 
-            System.out.print(Arrays.toString(arry));
+//       boolean x= PalindromeCheck.isPalindrome("level");
+//       if(x==true){
+//           System.out.println("Palindrome");
+//       }else {
+//           System.out.println("Not Palindrome");
+//       }
 
+        ///  ///////ReverseString ///////////////
+        String text = "hello";
+        System.out.println("رشته اصلی: " + text);
+        System.out.println("معکوس: " + ReverseString.reverse(text));
 
-
-
-
-
-
-
-
-
-
+        /// ///////////////////// TwoSumTwoPointers ////////////////////
+        int target = 9;
+        int[] result = TwoSumTwoPointers.TwoSum(arry, target);
+        if (result.length > 0) {
+            System.out.println("Numbers: " + result[0] + " , " + result[1]);
+        } else {
+            System.out.println("No solution found!");
+        }
     }
 }
